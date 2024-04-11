a8a4b222962c21efe030d653d0e919cad25d2f0c
Sravani Ramineni <musunurisravani34@gmail.com>
2024-04-11 09:06:06 +0200

    Add files via upload

---
+++ 20_simple_calculator.js
@@ -0,0 +1,22 @@
+//JavaScript Program to Make a Simple Calculator
+var num1=10;
+var num2=20;
+var result;
+var operator="+";
+
+if(operator=="+")
+{
+ result=num1+num2;
+}
+else if(operator=="-")
+{
+    result=num1-num2;
+}
+else if(operator=="*"){
+result=num1*num2;
+}
+else{
+    result=num1/num2;
+}
+
+console.log(num1+(operator)+num2+"="+result);
---
+++ 21_sum_of_natural.js
@@ -0,0 +1,8 @@
+//JavaScript Program to Find the Sum of Natural Numbers
+let num=4;
+let sum=0;
+for(i=1;i<num;i++)
+{
+    sum=sum+i;//1,3,6
+}
+console.log(sum);
